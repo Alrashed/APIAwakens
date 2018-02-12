d2f5e4f6140e19e5f95e7a4b1d644a0927f7a10e
Khalid Alrashed <Alrashed.kh@gmail.com>
2018-02-12 09:18:26 -0800

    Implement InfoCell conversion button actions

--- APIAwakens/Views/InfoCell.swift
+++ APIAwakens/Views/InfoCell.swift
@@ -8,23 +8,95 @@
 
 import UIKit
 
-class InfoCell: UITableViewCell {
-
+class InfoCell:  UITableViewCell {
     @IBOutlet weak var keyLabel: UILabel!
     @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var leftConversionButton: UIButton!
+    @IBOutlet weak var rightConversionButton: UIButton!
+    
+    var length: String?
+    var cost: String?
+    var exchangeRate: Double?
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
     }
     
     static var identifier: String {
         return String(describing: self)
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        if let length = length {
+            valueLabel.text = length
+            leftConversionButton.isHidden = false
+            leftConversionButton.setTitle("English", for: .normal)
+            rightConversionButton.isHidden = false
+            rightConversionButton.setTitle("Metric", for: .normal)
+        }
+
+        if let cost = cost {
+            valueLabel.text = cost
+            leftConversionButton.isHidden = false
+            leftConversionButton.setTitle("USD", for: .normal)
+            rightConversionButton.isHidden = false
+            rightConversionButton.setTitle("Credits", for: .normal)
+            
+            if let exchangeRate = exchangeRate, leftConversionButton.isSelected {
+                valueLabel.text = convertCreditsToUSD(credits: cost, exchangeRate: exchangeRate)
+            }
+        }
+    }
+    
+    @IBAction func leftConversionButtonTapped(_ sender: UIButton) {
+        if !sender.isSelected {
+            sender.isSelected = true
+            rightConversionButton.isSelected = false
+            
+            if sender.currentTitle == "English", let length = length {
+                valueLabel.text = convertMetricToEnglish(metric: length)
+            }
+            else if sender.currentTitle == "USD", let cost = cost, let exchangeRate = exchangeRate {
+                valueLabel.text = convertCreditsToUSD(credits: cost, exchangeRate: exchangeRate)
+            }
+        }
+    }
+    
+    @IBAction func rightConversionButtonTapped(_ sender: UIButton) {
+        if !sender.isSelected {
+            sender.isSelected = true
+            leftConversionButton.isSelected = false
+            
+            if sender.currentTitle == "Metric", let length = length {
+                valueLabel.text = length
+            }
+            else if sender.currentTitle == "Credits", let cost = cost {
+                valueLabel.text = cost
+            }
+        }
+    }
+    
+    func convertMetricToEnglish(metric: String) -> String {
+        if let metricValue = Double(metric) {
+            let englishValue = metricValue / 0.3048
+            
+            return String(format: "%.2f", englishValue)
+        }
+        
+        return metric
+    }
+    
+    func convertCreditsToUSD(credits: String, exchangeRate: Double) -> String {
+        if let credits = Double(credits) {
+            let usd = (credits * exchangeRate)
+            
+            return String(format: "%.2f", usd)
+        }
+        
+        return credits
+    }
 }
+
